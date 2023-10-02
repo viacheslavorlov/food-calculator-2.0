d783331d43abacd256d7260b43420fa175fa833d
viacheslav.orlov <viacheslavorlov@gmail.com>
2023-10-02 23:56:33 +0300

    storybook fixed

--- src/widgets/SearchWiget/ui/SearchWidget.stories.tsx
+++ src/widgets/SearchWiget/ui/SearchWidget.stories.tsx
@@ -2,20 +2,27 @@ import {ComponentMeta, ComponentStory} from '@storybook/react';
 import {ThemeConsts} from 'widgets/themeSwitcher';
 import {ThemeDecorator} from 'shared/helpers/testHelpers/StoryBookDecorators';
 import {SearchWidget} from './SearchWidget';
+import {StoreProvider} from 'app/reduxStore/StoreProvider/StoreProvider';
 
 export default {
-	title: 'shared/SearchWiget',
+	title: 'shared/SearchWidget',
 	component: SearchWidget,
 	argTypes: {
 		background: {control: 'background'},
 	},
+	decorators: [
+		ThemeDecorator(ThemeConsts.light)
+	]
 } as ComponentMeta<typeof SearchWidget>;
 
-const Template: ComponentStory<typeof SearchWidget> = (args) => <SearchWidget {...args} />;
+const Template: ComponentStory<typeof SearchWidget> = (args) => (
+	<StoreProvider initialState={{searchProducts: {searchValue: '', searchOrder: 'asc', searchProp: 'name'}}}>
+		<SearchWidget {...args} />
+	</StoreProvider>);
 
-export const LightSearchWiget = Template.bind({});
-LightSearchWiget.args = {};
+export const LightSearchWidget = Template.bind({});
+LightSearchWidget.args = {};
 
-export const DarkSearchWiget = Template.bind({});
-DarkSearchWiget.args = {};
-DarkSearchWiget.decorators = [ThemeDecorator(ThemeConsts.dark)];
+export const DarkSearchWidget = Template.bind({});
+DarkSearchWidget.args = {};
+DarkSearchWidget.decorators = [ThemeDecorator(ThemeConsts.dark)];
